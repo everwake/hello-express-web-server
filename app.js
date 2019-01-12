e48a7a390c1facbf66281c950a4026feec1cfee4
sajjad ahmadi <avarzaman.sajjad@gmail.com>
2019-01-13 01:14:07 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partial');
+
+hbs.registerHelper('getCurrentYear', ()=> {
+  return new Date().getFullYear()
+});
+//app.use("view engine", "hbs");
+
+// app.use((req, res)=> {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next)=> {
+  let now = new Date().toString();
+  console.log(`${now}: ${req.method}: ${req.url}`)
+  next();
+});
+
+//make a handler for get http request
+app.get("/", (req, res)=> {
+    // send response for http request
+    res.send("Hello Express");
+
+    // app.get("/help", (rq, res)=> {
+    //   res.sendFile(__dirname + '/public/help.html')
+    // })
+
+    app.get('/about', (req, res) => {
+        res.render('about.hbs', {
+          pageTitle: 'About Page.'
+        });
+      });
+
+    app.get('/main', (req,res) => {
+      res.render('main.hbs', {
+        pageTitle: 'Main Page'
+      }) 
+    })
+    
+
+    
+})
+
+
+app.listen(3000);
